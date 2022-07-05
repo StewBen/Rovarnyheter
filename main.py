c7ac0cc22a4adb2b0b20eda83de159c44c12be6e
Viktor Stubbfält <viktor.stubbfalt@gmail.com>
2022-07-05 22:57:31 +0200

    Added actual config vars.

--- main.py
+++ main.py
@@ -3,16 +3,14 @@
 import os
 import tweepy
 
-print(os.getenv("TEST"))
-
 ### Authentication:
-API_KEY = "key"
-API_KEY_SECRET = "key"
-CLIENT_ID = "key"
-CLIENT_ID_SECRET = "key"
-ACCESS_TOKEN = "key"
-ACCESS_TOKEN_SECRET = "key"
-BEARER_TOKEN = "key"
+API_KEY = os.getenv("API_KEY")
+API_KEY_SECRET = os.getenv("API_KEY_SECRET")
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_ID_SECRET = os.getenv("CLIENT_ID_SECRET")
+ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
+ACCESS_TOKEN_SECRET = os.getenv("ACCESS_TOKEN_SECRET")
+BEARER_TOKEN = os.getenv("BEARER_TOKEN")
 
 client = tweepy.Client(consumer_key = API_KEY,
                        consumer_secret = API_KEY_SECRET,
